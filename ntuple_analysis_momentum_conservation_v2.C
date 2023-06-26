08d5978d7b2c1ff713d7a09b513d8918cffb5500
JarnoVierros <vierros.jarno@gmail.com>
2023-06-26 16:43:38 +0200

    Improved dxy cut and added fits

--- ntuple_analysis_momentum_conservation_v2.C
+++ ntuple_analysis_momentum_conservation_v2.C
@@ -1,4 +1,5 @@
 
+const float peak = 780;//722.311
 
 string create_interval(float start, float stop) {
     return "((("+to_string(start)+"<x) ? 1 : 0) - (("+to_string(stop)+"<x) ? 1 : 0))";
@@ -186,23 +187,46 @@ class Event {
             }
             return variance;
         }
+
+        float get_squared_total_dxy() {
+            float total_squared_dxy = 0;
+            for (int i=0; i<particle_count; ++i) {
+                Particle* current_particle = *(particles+i);
+                total_squared_dxy += pow(current_particle->dxy, 2);
+            }
+            return total_squared_dxy;
+        }
+
+        float get_squared_total_dz() {
+            float total_squared_dz = 0;
+            for (int i=0; i<particle_count; ++i) {
+                Particle* current_particle = *(particles+i);
+                total_squared_dz += pow(current_particle->dz, 2);
+            }
+            return total_squared_dz;
+        }
 };
 
-Double_t func(float x,Double_t *par){
+Double_t func_gaussian(float x,Double_t *par){
     Double_t value = par[0]*TMath::Gaus(x,par[1],par[2]);
-    /*
-    cout << "x: " << x << endl;
-    cout << "par[0]: " << par[0] << endl;
-    cout << "par[1]: " << par[1] << endl;
-    cout << "par[2]: " << par[2] << endl;
-    cout << "VALUE: " << value << endl << endl;
-    */
+    return value;
+}
+
+Double_t func_BreitWigner(float x,Double_t *par){
+    Double_t value = par[0]*TMath::BreitWigner(x,par[1],par[2]);
+    return value;
+}
+
+Double_t func_Landau(float x,Double_t *par){
+    Double_t value = par[0]*TMath::Landau(x,par[1],par[2]);
     return value;
 }
 
 
 static TH1D* projection;
-void fcn(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag){
+static TH1D* raw_projection;
+
+void fcn_gaussian(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag){
     const Int_t nbins = 200;
     Int_t i;
 
@@ -213,26 +237,83 @@ void fcn(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag){
         if (projection->GetBinContent(i)<1) {
             continue;
         } 
-        delta  = enforce_interval(projection->GetBinCenter(i), 650, 850)*(projection->GetBinContent(i)-func(projection->GetBinCenter(i),par))/projection->GetBinError(i);
+        delta  = enforce_interval(projection->GetBinCenter(i), peak-100, peak+100)*(projection->GetBinContent(i)-func_gaussian(projection->GetBinCenter(i),par))/projection->GetBinError(i);
         //cout << "delta " << i << ": " << delta << endl;
         chisq += delta*delta;
     }
     f = chisq;
-    cout << "chisq: " << chisq << endl << endl;
+    //cout << "chisq: " << chisq << endl << endl;
+}
+
+void raw_fcn_gaussian(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag){
+    const Int_t nbins = 200;
+    Int_t i;
+
+    //calculate chisquare
+    Double_t chisq = 0;
+    Double_t delta;
+    for (i=0;i<nbins; i++) {
+        if (raw_projection->GetBinContent(i)<1) {
+            continue;
+        } 
+        delta  = enforce_interval(raw_projection->GetBinCenter(i), peak-100, peak+100)*(raw_projection->GetBinContent(i)-func_gaussian(raw_projection->GetBinCenter(i),par))/raw_projection->GetBinError(i);
+        //cout << "delta " << i << ": " << delta << endl;
+        chisq += delta*delta;
+    }
+    f = chisq;
+    //cout << "chisq: " << chisq << endl << endl;
+}
+
+void fcn_BreitWigner(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag){
+    const Int_t nbins = 200;
+    Int_t i;
+
+    //calculate chisquare
+    Double_t chisq = 0;
+    Double_t delta;
+    for (i=0;i<nbins; i++) {
+        if (projection->GetBinContent(i)<1) {
+            continue;
+        } 
+        delta  = enforce_interval(projection->GetBinCenter(i), peak-100, peak+100)*(projection->GetBinContent(i)-func_BreitWigner(projection->GetBinCenter(i),par))/projection->GetBinError(i);
+        //cout << "delta " << i << ": " << delta << endl;
+        chisq += delta*delta;
+    }
+    f = chisq;
+    //cout << "chisq: " << chisq << endl << endl;
+}
+
+void fcn_Landau(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag){
+    const Int_t nbins = 200;
+    Int_t i;
+
+    //calculate chisquare
+    Double_t chisq = 0;
+    Double_t delta;
+    for (i=0;i<nbins; i++) {
+        if (projection->GetBinContent(i)<1) {
+            continue;
+        } 
+        delta  = enforce_interval(projection->GetBinCenter(i), peak-100, 1400)*(projection->GetBinContent(i)-func_Landau(projection->GetBinCenter(i),par))/(projection->GetBinError(i));
+        //cout << "delta " << i << ": " << delta << endl;
+        chisq += delta*delta;
+    }
+    f = chisq;
+    //cout << "chisq: " << chisq << endl << endl;
 }
 
 void ntuple_analysis_momentum_conservation_v2() {
 
-    const string filename = "TOTEM43.root"; //"TOTEM43.root", kpkm.roo, 110000.root, rho.root, MinBias.root
-    const bool monte_carlo = false;
+    const string filename = "./ntuples/rho.root"; //"TOTEM43.root", kpkm.roo, 110000.root, rho.root, MinBias.root
+    const bool monte_carlo = true;
     const float pion_mass = 139.57039;
     const float rho_mass = 730; //770
 
     const float allowed_px_difference = 150;
     const float allowed_py_difference = 150;
     const float allowed_dxy_variance = 0.1;
-    const float allowed_dz_variance = 0.1;
-    const float allowed_rho_2_mass_difference = 100;
+    const float allowed_dz_variance = 0.6;
+    const float allowed_rho_2_mass_difference = 150;
 
     TFile *file = TFile::Open(filename.c_str());
     TTree* tree = (TTree*)file->Get("tree");
@@ -249,11 +330,11 @@ void ntuple_analysis_momentum_conservation_v2() {
     //auto total_dxy_vs_rho_m = new TH2F("total_dxy_vs_rho_m", "total dxy and rho mass;total dxy;rho mass/MeV",200,-5,5,200,200,1400);
     //auto total_dz_vs_rho_m = new TH2F("total_dz_vs_rho_m", "total dz and rho mass;total dz;rho mass/MeV",200,-5,5,200,200,1400);
 
-    auto dxy_variance_vs_rho_m1 = new TH2F("dxy_variance_vs_rho_m1", "dxy variance and rho1 mass;dxy variance;rho1 mass/MeV",200,0,5,200,200,1400);
-    auto dz_variance_vs_rho_m1 = new TH2F("dz_variance_vs_rho_m1", "dz variance and rho1 mass;dz variance;rho1 mass/MeV",200,0,5,200,200,1400);
+    auto total_dxy_squared_vs_rho_m1 = new TH2F("total_dxy_squared_vs_rho_m1", "total dxy^2 and rho1 mass;total dxy^2;rho1 mass/MeV",200,0,5,200,200,1400);
+    auto total_dz_squared_vs_rho_m1 = new TH2F("total_dz_squared_vs_rho_m1", "total dz^2 and rho1 mass;total dz^2;rho1 mass/MeV",200,0,5,200,200,1400);
 
-    auto dxy_variance_vs_rho_m2 = new TH2F("dxy_variance_vs_rho_m2", "dxy variance and rho2 mass;dxy variance;rho2 mass/MeV",200,0,5,200,200,1400);
-    auto dz_variance_vs_rho_m2 = new TH2F("dz_variance_vs_rho_m2", "dz variance and rho2 mass;dz variance;rho2 mass/MeV",200,0,5,200,200,1400);
+    auto total_dxy_squared_vs_rho_m2 = new TH2F("total_dxy_squared_vs_rho_m2", "total dxy^2 and rho2 mass;total dxy^2;rho2 mass/MeV",200,0,5,200,200,1400);
+    auto total_dz_squared_vs_rho_m2 = new TH2F("total_dz_squared_vs_rho_m2", "total dz^2 and rho2 mass;total dz^2;rho2 mass/MeV",200,0,5,200,200,1400);
 
     TTreeReaderArray<Float_t> trk_p(Reader, "trk_p");
     TTreeReaderArray<Float_t> trk_pt(Reader, "trk_pt");
@@ -366,11 +447,11 @@ void ntuple_analysis_momentum_conservation_v2() {
             }
             rho_masses_raw->Fill(raw_masses[0], raw_masses[1]);
 
-            dxy_variance_vs_rho_m1->Fill(current_event.get_dxy_variance(), raw_masses[0]);
-            dz_variance_vs_rho_m1->Fill(current_event.get_dz_variance(), raw_masses[0]);
+            total_dxy_squared_vs_rho_m1->Fill(current_event.get_squared_total_dxy(), raw_masses[0]);
+            total_dz_squared_vs_rho_m1->Fill(current_event.get_squared_total_dz(), raw_masses[0]);
 
-            dxy_variance_vs_rho_m2->Fill(current_event.get_dxy_variance(), raw_masses[1]);
-            dz_variance_vs_rho_m2->Fill(current_event.get_dz_variance(), raw_masses[1]);
+            total_dxy_squared_vs_rho_m2->Fill(current_event.get_squared_total_dxy(), raw_masses[1]);
+            total_dz_squared_vs_rho_m2->Fill(current_event.get_squared_total_dz(), raw_masses[1]);
         }
         
         if (allowed_px_difference < abs(abs(current_event.PR_p[0]+current_event.PL_p[0]) - abs(current_event.ref_p[0]))) {
@@ -430,44 +511,51 @@ void ntuple_analysis_momentum_conservation_v2() {
     //rho_masses_raw->ProjectionX("X_projection_raw")->Draw();
 
     auto raw_projections = new TCanvas("Canvas3","Canvas3");
-    rho_masses_raw->ProjectionX("X_projection_raw")->Draw();
+    raw_projection = rho_masses_raw->ProjectionX("X_projection_raw");
+    raw_projection->Draw();
+
+    float results[4];
+
+    TMinuit *gMinuit0 = new TMinuit(3);
+    gMinuit0->SetFCN(raw_fcn_gaussian);
+
+    Double_t arglist0[10];
+    Int_t ierflg0 = 0;
+
+    arglist0[0] = 1;
+
+    gMinuit0->mnexcm("SET ERR", arglist0 ,1,ierflg0);
+
+    static Double_t vstart0[4] = {1.92522e+03, 7.14645e+02, 1.24676e+02};
+    static Double_t step0[4] = {1, 1, 1};
+    gMinuit0->mnparm(0, "a0", vstart0[0], step0[0], 0,0,ierflg0);
+    gMinuit0->mnparm(1, "a2", vstart0[1], step0[1], 0,0,ierflg0);
+    gMinuit0->mnparm(2, "a3", vstart0[2], step0[2], 0,0,ierflg0);
+
+    arglist0[0] = 500;
+    arglist0[1] = 1.;
+    gMinuit0->mnexcm("MIGRAD", arglist0 ,2,ierflg0);
+
+    Double_t amin,edm,errdef;
+    Int_t nvpar,nparx,icstat;
+    gMinuit0->mnstat(amin,edm,errdef,nvpar,nparx,icstat);
+
+    Double_t val1,err1,val2,err2,val3,err3;
+    gMinuit0->GetParameter(0, val1, err1);
+    gMinuit0->GetParameter(1, val2, err2);
+    results[0] = val2;
+    gMinuit0->GetParameter(2, val3, err3);
+
+    TF1 gaussia_fit_raw("gaussia_fit_raw", "[0]*TMath::Gaus(x,[1],[2])", 200, 1400);
+    gaussia_fit_raw.SetParameters(val1, val2, val3);
+
+    gaussia_fit_raw.DrawCopy("Same");
+
 
     auto main = new TCanvas("Canvas4","Canvas4");
     rho_masses->Draw("Colz");
 
-    /*
-    const int k2_x_min = 450;
-    const int k2_x_max = 550;
-    const int k2_y_min = 200;
-    const int k2_y_max = 800;
-
-    TLine k2_line1 = TLine(k2_x_min, k2_y_min, k2_x_max, k2_y_min);
-    k2_line1.DrawClone();
-
-    TLine k2_line2 = TLine(k2_x_max, k2_y_min, k2_x_max, k2_y_max);
-    k2_line2.DrawClone();
-
-    TLine k2_line3 = TLine(k2_x_min, k2_y_max, k2_x_max, k2_y_max);
-    k2_line3.DrawClone();
-
-    TLine k2_line4 = TLine(k2_x_min, k2_y_min, k2_x_min, k2_y_max);
-    k2_line4.DrawClone();
-
-
-    TCutG *k2_cut = new TCutG("k2_cut",4);
-    k2_cut->SetPoint(0,k2_x_min,k2_y_min);
-    k2_cut->SetPoint(1,k2_x_max,k2_y_min);
-    k2_cut->SetPoint(2,k2_x_max,k2_y_max);
-    k2_cut->SetPoint(3,k2_x_min,k2_y_max);
-    k2_cut->SetPoint(4,k2_x_min,k2_y_min);
-
-
-    TLine ycut_line1 = TLine(200, 600, 1400, 600);
-    ycut_line1.DrawClone();
-
-    TLine ycut_line2 = TLine(200, 800, 1400, 800);
-    ycut_line2.DrawClone();
-    */
+ 
 
     auto projections = new TCanvas("Canvas5","Canvas5");
     
@@ -477,19 +565,137 @@ void ntuple_analysis_momentum_conservation_v2() {
     projection->Draw();
 
 
+/////////////////////////////////////
+
+    TMinuit *gMinuit1 = new TMinuit(3);
+    gMinuit1->SetFCN(fcn_gaussian);
+
+    Double_t arglist1[10];
+    Int_t ierflg1 = 0;
+
+    arglist1[0] = 1;
+
+    gMinuit1->mnexcm("SET ERR", arglist1 ,1,ierflg1);
+
+    static Double_t vstart1[4] = {6.16160e+02, 7.22303e+02, 1.17525e+02};
+    static Double_t step1[4] = {1, 1, 1};
+    gMinuit1->mnparm(0, "a1", vstart1[0], step1[0], 0,0,ierflg1);
+    gMinuit1->mnparm(1, "a2", vstart1[1], step1[1], 0,0,ierflg1);
+    gMinuit1->mnparm(2, "a3", vstart1[2], step1[2], 0,0,ierflg1);
+
+    arglist1[0] = 500;
+    arglist1[1] = 1.;
+    gMinuit1->mnexcm("MIGRAD", arglist1 ,2,ierflg1);
+
+    gMinuit1->mnstat(amin,edm,errdef,nvpar,nparx,icstat);
+
+    
+    gMinuit1->GetParameter(0, val1, err1);
+    gMinuit1->GetParameter(1, val2, err2);
+    results[1] = val2;
+    gMinuit1->GetParameter(2, val3, err3);
+
+    TF1 gaussia_fit("gaussia_fit", "[0]*TMath::Gaus(x,[1],[2])", 200, 1400);
+    gaussia_fit.SetParameters(val1, val2, val3);
+
+    gaussia_fit.DrawCopy("Same");
+
+    
+////////////////////////////////////////////
+    
+    TMinuit *gMinuit2 = new TMinuit(3);
+    gMinuit2->SetFCN(fcn_BreitWigner);
+
+    Double_t arglist2[10];
+    Int_t ierflg2 = 0;
+
+    arglist2[0] = 1;
+
+    gMinuit2 ->mnexcm("SET ERR", arglist2 ,1,ierflg2);
+
+    static Double_t vstart2[4] = {3.26161e+05, 7.11412e+02, 3.42154e+02};
+    static Double_t step2[4] = {1, 1, 1};
+    gMinuit2 ->mnparm(0, "a1", vstart2[0], step2[0], 0,0,ierflg2);
+    gMinuit2 ->mnparm(1, "a2", vstart2[1], step2[1], 0,0,ierflg2);
+    gMinuit2 ->mnparm(2, "a3", vstart2[2], step2[2], 0,0,ierflg2);
+
+    arglist2[0] = 500;
+    arglist2[1] = 1.;
+    gMinuit2 ->mnexcm("MIGRAD", arglist2 ,2,ierflg2);
+
+    //Double_t amin,edm,errdef;
+    //Int_t nvpar,nparx,icstat;
+    gMinuit2 ->mnstat(amin,edm,errdef,nvpar,nparx,icstat);
+
+    //Double_t val1,err1,val2,err2,val3,err3;
+    gMinuit2 ->GetParameter(0, val1, err1);
+    gMinuit2 ->GetParameter(1, val2, err2);
+    results[2] = val2;
+    gMinuit2 ->GetParameter(2, val3, err3);
+
+    TF1 BreitWigner_fit("BreitWigner_fit", "[0]*TMath::BreitWigner(x,[1],[2])", 200, 1400);
+    BreitWigner_fit.SetParameters(val1, val2, val3);
+    BreitWigner_fit.SetLineColor(3);
+    BreitWigner_fit.DrawCopy("Same");
+
+
+////////////////////////////////////////////
+
+    TMinuit *gMinuit3 = new TMinuit(3);
+    gMinuit3->SetFCN(fcn_Landau);
+
+    Double_t arglist3[10];
+    Int_t ierflg3 = 0;
+
+    arglist3[0] = 1;
+
+    gMinuit3 ->mnexcm("SET ERR", arglist3 ,1,ierflg3);
+
+    static Double_t vstart3[4] = {3.28034e+03, 7.12024e+02, 7.82408e+01};
+    static Double_t step3[4] = {1, 0.1, 1};
+    gMinuit3 ->mnparm(0, "a1", vstart3[0], step3[0], 0,0,ierflg3);
+    gMinuit3 ->mnparm(1, "a2", vstart3[1], step3[1], 0,0,ierflg3);
+    gMinuit3 ->mnparm(2, "a3", vstart3[2], step3[2], 0,0,ierflg3);
+
+    arglist3[0] = 500;
+    arglist3[1] = 1.;
+    gMinuit3 ->mnexcm("MIGRAD", arglist3 ,2,ierflg3);
+
+    //Double_t amin,edm,errdef;
+    //Int_t nvpar,nparx,icstat;
+    gMinuit3 ->mnstat(amin,edm,errdef,nvpar,nparx,icstat);
+
+    gMinuit3 ->GetParameter(0, val1, err1);
+    gMinuit3 ->GetParameter(1, val2, err2);
+    results[3] = val2;
+    gMinuit3 ->GetParameter(2, val3, err3);
+
+    TF1 Landau_fit("Landau_fit", "[0]*TMath::Landau(x,[1],[2])", 200, 1400);
+    Landau_fit.SetParameters(val1, val2, val3);
+    Landau_fit.SetLineColor(4);
+    Landau_fit.DrawCopy("Same");
+
+
+
     
 
     auto dxy_comparison_1 = new TCanvas("Canvas6","Canvas6");
-    dxy_variance_vs_rho_m1->Draw("Colz");
+    total_dxy_squared_vs_rho_m1->Draw("Colz");
 
     auto dz_comparison_1 = new TCanvas("Canvas7","Canvas7");
-    dz_variance_vs_rho_m1->Draw("Colz");
+    total_dz_squared_vs_rho_m1->Draw("Colz");
 
     auto dxy_comparison_2 = new TCanvas("Canvas8","Canvas8");
-    dxy_variance_vs_rho_m2->Draw("Colz");
+    total_dxy_squared_vs_rho_m2->Draw("Colz");
 
     auto dz_comparison_2 = new TCanvas("Canvas9","Canvas9");
-    dz_variance_vs_rho_m2->Draw("Colz");
+    total_dz_squared_vs_rho_m2->Draw("Colz");
+
+    cout << "RESULTS: " << endl;
+    cout << results[0] << endl;
+    cout << results[1] << endl;
+    cout << results[2] << endl;
+    cout << results[3] << endl;
 
     /*
     TMinuit *gMinuit = new TMinuit(3);
@@ -536,3 +742,11 @@ void ntuple_analysis_momentum_conservation_v2() {
     gaussia_fit.DrawCopy("Same");
     */   
 }
+
+/*
+new TOTEM43
+710.461
+720.492
+721.018
+713.57
+*/
